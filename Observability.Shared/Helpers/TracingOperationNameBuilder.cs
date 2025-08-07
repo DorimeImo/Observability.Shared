c63d1a8a8f09f9b47b9c14976232de6a810b6b86
D B <dmitry.baranov.mercury@gmail.com>
2025-08-07 17:46:44 +0200

    added TracingNameBuilder to Helpers folder

---
+++ Observability.Shared/Helpers/TracingOperationNameBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observability.Shared.Helpers
+{
+    public static class TracingOperationNameBuilder
+    {
+        public static string TracingOperationNameBuild((string Source, string Operation) callerInfo)
+            => $"{callerInfo.Source}.{callerInfo.Operation}";
+    }
+}
